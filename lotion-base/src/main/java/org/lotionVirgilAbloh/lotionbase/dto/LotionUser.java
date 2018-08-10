5bf95f1e60abaf28083bc8f6b45a7817a6cd2faa
pengbo <pengbo@drplant.com.cn>
2018-08-10 15:36:31 +0800

    lotinuser add in authcenter use feign

---
+++ lotion-base/src/main/java/org/lotionVirgilAbloh/lotionbase/dto/LotionUser.java
@@ -0,0 +1,24 @@
+package org.lotionVirgilAbloh.lotionbase.dto;
+
+public class LotionUser {
+
+    private String userName;
+
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
